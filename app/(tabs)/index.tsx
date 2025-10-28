a9ee610df157de67682b3c94fe354fa3ac111131
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-10-28 14:51:19 +0900

    [fix] webview load 전에 ActivityIndicator 보여주도록 수정 (#3)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,28 +1,38 @@
+import { useState } from "react";
+
 import { WebviewEvent } from "@/constants/webview";
 import { useFcm } from "@/hooks/use-fcm";
 import { useWebView } from "@/hooks/use-webview";
 import { api } from "@/lib/ky";
 import { useTokenStore } from "@/lib/zustand/user";
 
-import { Linking, StyleSheet } from "react-native";
+import { ActivityIndicator, Linking, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { WebView, WebViewMessageEvent } from "react-native-webview";
+import WebView, { WebViewMessageEvent } from "react-native-webview";
 
 const SERVICE_INTRODUCTION_URL = process.env.EXPO_PUBLIC_SERVICE_INTRODUCTION_URL ?? "";
 const WEBVIEW_URL = process.env.EXPO_PUBLIC_WEBVIEW_URL ?? "";
 
 export default function WebviewScreen() {
+  const [isWebReady, setIsWebReady] = useState(false);
+
   const { webviewRef, postMessage } = useWebView();
 
   const setToken = useTokenStore((state) => state.setToken);
 
   const { getDeviceToken, requestUserPermission } = useFcm();
 
+  const onLoad = () => {
+    setIsWebReady(true);
+  };
+
   const onMessage = async (e: WebViewMessageEvent) => {
     const { type, data } = JSON.parse(e.nativeEvent.data);
+
     if (type === WebviewEvent.OPEN_SETTING) {
       return Linking.openSettings();
     }
+
     if (type === WebviewEvent.GET_ALARM_STATUS) {
       const { accessToken, refreshToken } = data;
 
@@ -36,15 +46,28 @@ export default function WebviewScreen() {
       postMessage(WebviewEvent.GET_ALARM_STATUS, { alarmEnabled });
       return;
     }
+
     if (type === WebviewEvent.OPEN_SERVICE_INTRODUCTION) {
       return Linking.openURL(SERVICE_INTRODUCTION_URL);
     }
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <WebView source={{ uri: WEBVIEW_URL }} ref={webviewRef} onMessage={onMessage} />
-    </SafeAreaView>
+    <>
+      {!isWebReady && (
+        <View style={styles.activityIndicatorContainer}>
+          <ActivityIndicator size="large" />
+        </View>
+      )}
+      <SafeAreaView style={styles.container}>
+        <WebView
+          source={{ uri: WEBVIEW_URL }}
+          ref={webviewRef}
+          onLoad={onLoad}
+          onMessage={onMessage}
+        />
+      </SafeAreaView>
+    </>
   );
 }
 
@@ -52,5 +75,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
+  },
+  activityIndicatorContainer: {
+    flex: 1,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 100,
   },
 });
