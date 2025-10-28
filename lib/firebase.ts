f158326790dcb879667d7b56ee9ed3c141769030
gs0428 <lider28@naver.com>
2025-10-28 11:38:07 +0900

    fix: android에서 firebase 초기화 되지 않던 오류 해결

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,8 @@
-import { getApp } from "@react-native-firebase/app";
+import { convertGoogleServiceInfoToFirebaseConfig } from "@/utils/firebase";
+import { getApp, initializeApp } from "@react-native-firebase/app";
 import {
   AuthorizationStatus,
+  FirebaseMessagingTypes,
   getMessaging,
   getToken,
   requestPermission,
@@ -8,16 +10,40 @@ import {
 
 import { PermissionsAndroid, Platform } from "react-native";
 
-const app = getApp();
-const messaging = getMessaging(app);
+let messaging: FirebaseMessagingTypes.Module | null = null;
+let appInitialized = false;
+
+// firebase 초기화
+export const initFirebase = async () => {
+  if (appInitialized) return;
+
+  try {
+    let app;
+    // android인 경우 json 파일을 이용해 초기화
+    if (Platform.OS === "android") {
+      app = await initializeApp(convertGoogleServiceInfoToFirebaseConfig());
+    } else {
+      // 그 외의 경우는 기존 앱 인스턴스 사용
+      app = getApp();
+    }
+
+    messaging = getMessaging(app);
+    appInitialized = true;
+  } catch (e) {
+    console.error("Firebase init failed:", e);
+  }
+};
 
 // 기기 토큰 추출
 export const getDeviceToken = async () => {
-  return await getToken(messaging);
+  if (!messaging) await initFirebase();
+  return await getToken(messaging!);
 };
 
 // 푸시 알람 권한 요청
 export const requestUserPermission = async (): Promise<boolean> => {
+  if (!messaging) await initFirebase();
+
   if (Platform.OS === "android") {
     const hasPermission = await PermissionsAndroid.check(
       PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
@@ -32,8 +58,7 @@ export const requestUserPermission = async (): Promise<boolean> => {
   }
 
   if (Platform.OS === "ios") {
-    const authStatus = await requestPermission(messaging);
-
+    const authStatus = await requestPermission(messaging!);
     return (
       authStatus === AuthorizationStatus.AUTHORIZED ||
       authStatus === AuthorizationStatus.PROVISIONAL
