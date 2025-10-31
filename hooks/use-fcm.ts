be0dcb3033b75d1771a2ebfba8b52fc52063b3bc
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-10-31 12:05:08 +0900

    [fix] google service 파일 가져오는 로직 제거 (#5)

--- hooks/use-fcm.ts
+++ hooks/use-fcm.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-import { getApp, initializeApp } from "@react-native-firebase/app";
+import { getApp } from "@react-native-firebase/app";
 import {
   AuthorizationStatus,
   FirebaseMessagingTypes,
@@ -11,19 +11,6 @@ import {
 } from "@react-native-firebase/messaging";
 
 import { PermissionsAndroid, Platform } from "react-native";
-
-import googleServiceInfo from "../google-services.json";
-
-function convertGoogleServiceInfoToFirebaseConfig() {
-  return {
-    appId: googleServiceInfo.client[0].client_info.mobilesdk_app_id,
-    projectId: googleServiceInfo.project_info.project_id,
-    messagingSenderId: googleServiceInfo.project_info.project_number,
-    apiKey: googleServiceInfo.client[0].api_key[0].current_key,
-    storageBucket: googleServiceInfo.project_info.storage_bucket,
-    databaseURL: "",
-  };
-}
 
 function onMessageReceived(messaging: FirebaseMessagingTypes.Module) {
   onMessage(messaging, async (remoteMessage) => {
@@ -71,14 +58,9 @@ export const useFcm = () => {
   useEffect(() => {
     const initFirebase = async () => {
       try {
-        let app = getApp();
-
-        // android이고 초기화가 되지 않은 경우 json 파일을 이용해 초기화
-        if (Platform.OS === "android" && app === null) {
-          app = await initializeApp(convertGoogleServiceInfoToFirebaseConfig());
-        }
-
+        const app = getApp();
         const initializedMessaging = getMessaging(app);
+
         setMessaging(initializedMessaging);
         onMessageReceived(initializedMessaging);
       } catch (e) {
