e132ee9c9e6afaae73a6019e9b4c38f54e639a50
gs0428 <lider28@naver.com>
2026-05-05 22:20:25 +0900

    feat: revenuecat 결제 구현

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { WebviewEvent } from "@/constants/webview";
 import { useFcm } from "@/hooks/use-fcm";
 import { useWebView } from "@/hooks/use-webview";
+import type { OrderProps } from "@/types/order";
 
-import { ActivityIndicator, Linking, StyleSheet, View } from "react-native";
+import { ActivityIndicator, Linking, Platform, StyleSheet, View } from "react-native";
+import Purchases, {
+  LOG_LEVEL,
+  PURCHASES_ERROR_CODE,
+  type PurchasesError,
+} from "react-native-purchases";
 import WebView, { type WebViewMessageEvent } from "react-native-webview";
 
 const SERVICE_INTRODUCTION_URL = process.env.EXPO_PUBLIC_SERVICE_INTRODUCTION_URL ?? "";
 const WEBVIEW_URL = process.env.EXPO_PUBLIC_WEBVIEW_URL ?? "";
+
+const IOS_PURCHASES_API_KEY = process.env.EXPO_PUBLIC_IOS_PURCHASES_API_KEY ?? "";
+const ANDROID_PURCHASES_API_KEY = process.env.EXPO_PUBLIC_ANDROID_PURCHASES_API_KEY ?? "";
+
+const isPurchasesError = (error: unknown): error is PurchasesError => {
+  return (
+    typeof error === "object" &&
+    error !== null &&
+    "code" in error &&
+    "message" in error &&
+    typeof (error as { message?: unknown }).message === "string"
+  );
+};
 
 export default function WebviewScreen() {
   const [isWebReady, setIsWebReady] = useState(false);
@@ -22,7 +41,7 @@ export default function WebviewScreen() {
   };
 
   const onMessage = async (e: WebViewMessageEvent) => {
-    const { type } = JSON.parse(e.nativeEvent.data);
+    const { type, data } = JSON.parse(e.nativeEvent.data);
 
     if (type === WebviewEvent.OPEN_SETTING) {
       return Linking.openSettings();
@@ -45,7 +64,60 @@ export default function WebviewScreen() {
       postMessage(WebviewEvent.REGISTER_DEVICE_TOKEN, { fcmToken });
       return;
     }
+
+    if (type === WebviewEvent.OPEN_SERVICE_INTRODUCTION) {
+      return Linking.openURL(SERVICE_INTRODUCTION_URL);
+    }
+
+    if (type === WebviewEvent.GET_DEVICE_TOKEN) {
+      const fcmToken = await getDeviceToken();
+      if (fcmToken === null) return;
+
+      postMessage(WebviewEvent.REGISTER_DEVICE_TOKEN, { fcmToken });
+      return;
+    }
+
+    if (type === WebviewEvent.REQUEST_ORDER) {
+      const order = data as OrderProps;
+      const storeProductId = order.storeProductId;
+
+      try {
+        const storeProducts = await Purchases.getProducts([storeProductId]);
+        const targetStoreProduct = storeProducts[0];
+
+        if (targetStoreProduct === undefined) {
+          return;
+        }
+
+        const purchaseResult = await Purchases.purchaseStoreProduct(targetStoreProduct);
+        postMessage(WebviewEvent.REQUEST_ORDER_SUCCESS, {
+          orderId: order.orderId,
+          productIdentifier: storeProductId,
+          transactionIdentifier: purchaseResult.transaction.transactionIdentifier,
+        });
+      } catch (error: unknown) {
+        if (isPurchasesError(error)) {
+          if (error.code === PURCHASES_ERROR_CODE.PURCHASE_CANCELLED_ERROR) {
+            postMessage(WebviewEvent.REQUEST_ORDER_CANCELLED, { error: "결제가 취소되었습니다." });
+            return;
+          }
+        }
+        postMessage(WebviewEvent.REQUEST_ORDER_FAILED, { error });
+      }
+
+      return;
+    }
   };
+
+  useEffect(() => {
+    Purchases.setLogLevel(LOG_LEVEL.VERBOSE);
+
+    if (Platform.OS === "ios") {
+      Purchases.configure({ apiKey: IOS_PURCHASES_API_KEY });
+    } else if (Platform.OS === "android") {
+      Purchases.configure({ apiKey: ANDROID_PURCHASES_API_KEY });
+    }
+  }, []);
 
   return (
     <>
@@ -60,6 +132,7 @@ export default function WebviewScreen() {
           ref={webviewRef}
           onLoad={onLoad}
           onMessage={onMessage}
+          webviewDebuggingEnabled={__DEV__}
         />
       </View>
     </>
---
+++ types/order.ts
@@ -0,0 +1,8 @@
+export interface OrderProps {
+  amount: number;
+  orderId: number;
+  orderNumber: string;
+  productId: number;
+  productName: string;
+  storeProductId: string;
+}
